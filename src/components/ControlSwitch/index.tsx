be001b87af9dbef171869cdb381d8254db62e0b6
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-09-02 13:24:01 +0100

    build: replace icon pack with react-icons

---
+++ src/components/ControlSwitch/index.tsx
@@ -0,0 +1,13 @@
+import LineIcon from "react-lineicons";
+
+function ControlSwitch() {
+  return (
+    <div>
+      <button>
+        <LineIcon name="spinner" />
+      </button>
+    </div>
+  );
+}
+
+export default ControlSwitch;
