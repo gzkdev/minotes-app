35f64f533180a88c55dd01c5b5541e7c5668a887
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-09-02 13:08:44 +0100

    feature: setup routes for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 function App() {
-  return <h1>Home page</h1>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/notes/:id" element={<h1>Edit Note</h1>} />
+        <Route path="/favorites" element={<h1>Favorite Notes</h1>} />
+        <Route path="/recent" element={<h1>Recent Notes</h1>} />
+        <Route path="/trash" element={<h1>deleted Notes</h1>} />
+        <Route path="*" element={<h1>404 Page</h1>} />
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
