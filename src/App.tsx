02ffc7e0b9cea0be9474302bb041ce7ebe5058c5
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-09-02 13:13:27 +0100

    chore: add home page component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages";
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<h1>Home Page</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/notes/:id" element={<h1>Edit Note</h1>} />
         <Route path="/favorites" element={<h1>Favorite Notes</h1>} />
         <Route path="/recent" element={<h1>Recent Notes</h1>} />
