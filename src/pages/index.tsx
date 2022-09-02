90de337e63d0c2d1241466cb7e3855415dded648
Ezihe Godswill <ezihegodswill01@gmail.com>
2022-09-02 13:57:31 +0100

    style: add home styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,13 @@
 import { ControlSwitch } from "../components";
+import styles from "../styles/HomePage.module.css";
 function Home() {
   return (
-    <div>
-      <ControlSwitch />
-      <h1>Home Page</h1>
+    <div className={styles.container}>
+      <div className={styles.main} data-role="main">
+        <ControlSwitch />
+        <h1>Home Page</h1>
+      </div>
+      <div className={styles.space} data-role="section"></div>
     </div>
   );
 }
