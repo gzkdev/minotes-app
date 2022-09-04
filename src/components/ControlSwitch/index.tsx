f9f8f7f85ec3a12dc0cd765a08771f8e4e4e3707
gzkdev <ezihegodswill01@gmail.com>
2022-09-04 19:52:05 +0100

    style: add styles for control switch ui states

--- src/components/ControlSwitch/index.tsx
+++ src/components/ControlSwitch/index.tsx
@@ -1,10 +1,16 @@
+import { useSelector,useDispatch } from "react-redux";
 import styles from "./ControlSwitch.module.css";
+import { toggleControl } from "../../store/ControlSlice";
+import { RootState } from "../../store";
 
 function ControlSwitch() {
+  const dispatch = useDispatch();
+  const controlState = useSelector((state: RootState) => state.control.control);
+  
   return (
-    <div className={styles.control__switch} data-mode="notes">
-      <button className={styles.control__btn}>Notes</button>
-      <button className={styles.control__btn}>Lists</button>
+    <div className={styles.control__switch} data-mode={controlState}>
+      <button className={styles.control__btn} onClick={()=>dispatch(toggleControl())}>Notes</button>
+      <button className={styles.control__btn} onClick={()=>dispatch(toggleControl())}>Lists</button>
     </div>
   );
 }
